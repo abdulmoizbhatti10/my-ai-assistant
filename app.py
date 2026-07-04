b37ec91ee8f028b51f82122711cec7e63c12bfba
abdulmoizbhatti10 <abdulmoizbhatti10@gmail.com>
2026-07-05 01:53:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ else:
     st.error("API Key missing! Please set it in Streamlit Cloud Secrets.")
 
 # Model selection
-model = model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Chat session state
 if "messages" not in st.session_state:
